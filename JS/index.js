9f762adc35bd2984075b56ff1aeceb52c38d78fb
=bibin thomas <bibinthomas951@gmail.com>
2022-06-02 20:54:33 +0530

    Main Javascript

--- JS/index.js
+++ JS/index.js
@@ -1,28 +1,33 @@
-// let Storynumber = ["story1.html", "story2.html", "story3.html"];
+//array containing our pages
+const pages = ["story1.html", "story2.html", "story3.html","story4.html"];
 
-let currentLocation = window.location;
-var i;
+function redirectToNextPage(){
+    //get the current page - this grabs just the last part of the URL (e.g. "story1.html" instead of "HTML/story1.html"
+    const curr = window.location.pathname.split('/').pop();
 
-window.onbeforeunload = function () {
-  if (currentLocation.pathname == "/HTML/story1.html") {
-    window.setTimeout(function () {
-      window.location.replace("http://127.0.0.1:5500/HTML/story2.html");
-      console.log("Working");
-  });
-    window.onbeforeunload = null; // necessary to prevent infinite loop, that kills your browser
-  } else if (currentLocation.pathname == "/HTML/story2.html") {
-    window.setTimeout(function () {
-      window.location.replace("http://127.0.0.1:5500/HTML/story3.html");
-      console.log("Working");
-    });
-    window.onbeforeunload = null;
-  } else if (currentLocation.pathname == "/HTML/story3.html") {
-    window.setTimeout(function () {
-      window.location.replace("http://127.0.0.1:5500/HTML/story1.html");
-      console.log("Working");
-    });
-    window.onbeforeunload = null;
-  } else {
-    console.log("same page cant be reloaded");
-  }
+    //work out which page we're on - if curr is "story1.html", this is 0, if curr is "story2.html" this is 1, etc. If it doesn't match, this is -1
+    let index = pages.indexOf(curr);
+
+    if(index < 0 || index >= pages.length){ //if we're either non-matching or on the last element
+      console.log(index)
+        index = 0; //reset to the first one
+    } 
+    else {
+      if(index<4)
+        index++; //go to the next one
+    }
+
+    // window.removeEventListener('beforeunload', redirectToNextPage); //remove event listener
+    window.location.replace(pages[index]); //do the redirect to the next page
 };
+
+// window.addEventListener('beforeunload', redirectToNextPage);
+
+
+// if (window.performance) {
+//   console.info("window.performance works fine on this browser");
+// }
+// console.info(performance.navigation.type);
+if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+   redirectToNextPage();
+} 
